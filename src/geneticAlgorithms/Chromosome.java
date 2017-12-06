5fe652e7791476be96068da27194e546f07276d5
WenqiCui <cui.wenq@husky.neu.edu>
2017-12-06 16:33:30 -0500

    gene

---
+++ src/geneticAlgorithms/Chromosome.java
@@ -0,0 +1,21 @@
+package geneticAlgorithms;
+public class Chromosome {
+
+	public final static int CITY_NUMBER = GA_TSP.CITY_NUMBER;
+	public static int[][] MAP = GA_TSP.MAP;
+	int gene[] = new int[CITY_NUMBER];
+	int value;
+	public Chromosome(int[] gene) {
+		this.gene = gene.clone();
+		calValue();
+	}
+	private void calValue(){
+		value = 0;
+		for (int i = 1;i < CITY_NUMBER; i++){
+			value += MAP[gene[i-1]][gene[i]];
+		}
+		
+	}
+	
+	
+}
--- src/geneticAlgorithms/GA_TSP.java
+++ src/geneticAlgorithms/GA_TSP.java
@@ -1,6 +1,21 @@
 package geneticAlgorithms;
 
+import java.util.ArrayList;
+
 public class GA_TSP {
 
-	public static final int CITY_NUMBER = 10; 
+	public final static int CITY_NUMBER = 10; 
+	public static int[][] MAP = new int[CITY_NUMBER][CITY_NUMBER];
+	public ArrayList<Chromosome> Population = new ArrayList<Chromosome>();
+	
+	
+	
 }
+
+
+
+
+//class Chromosome{
+//	int[] Gene =new int[GA_TSP.CITY_NUMBER];
+//	
+//}
