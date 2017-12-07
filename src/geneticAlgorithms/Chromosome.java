b01f8010b4fa3ce4241c35749031849d4067c88e
WenqiCui <cui.wenq@husky.neu.edu>
2017-12-07 14:08:09 -0500

    Structure, initialization for GA_TSP class Random construction for Chromosome class

--- src/geneticAlgorithms/Chromosome.java
+++ src/geneticAlgorithms/Chromosome.java
@@ -1,25 +1,52 @@
 package geneticAlgorithms;
+
+import java.util.Random;
+
 public class Chromosome {
 
 	public final static int CITY_NUMBER = GA_TSP.CITY_NUMBER;
 	public static int[][] MAP = GA_TSP.MAP;
-	int gene[] = new int[CITY_NUMBER];
+	int gene[] ;
 	int distance;
+	/*
+	Construction
+	Create by an array
+	 */
 	public Chromosome(int[] gene) {
 		this.gene = gene.clone();
 		calDistance();
 	}
+	/*
+	Construction
+	Create by random
+	 */
 	public Chromosome() {
+		Random random = new Random();
+		gene = new int[CITY_NUMBER];
+		for (int i = 1; i < CITY_NUMBER; i++){
+			gene[i] = i;
+		}
+		int j = 0;
+		int t = 0;
+		for (int i = CITY_NUMBER-1; i > 0; i--){
+			j = random.nextInt(i)+1;
+			t = gene[i];
+			gene[i] = gene [j];
+			gene[j] = t;
+		}
 		calDistance();
 
 
 	}
+	/*
+	Calculate distance
+	 */
 	private void calDistance(){
 		distance = 0;
 		for (int i = 1;i < CITY_NUMBER; i++){
 			distance += MAP[gene[i-1]][gene[i]];
 		}
-		
+		distance += MAP[gene[CITY_NUMBER-1]][0];
 	}
 	
 	
--- src/geneticAlgorithms/GA_TSP.java
+++ src/geneticAlgorithms/GA_TSP.java
@@ -4,19 +4,58 @@ import java.util.ArrayList;
 
 public class GA_TSP {
 
-	public final static int CITY_NUMBER = 10; 
+	public static final int CITY_NUMBER = 10;
+	public static final int MAX_GENERATION = 100;
+	public static final int MAX_POPULATION = 100;
+	public static final int MAX_DISTANCE = 30000;
 	public static int[][] MAP = new int[CITY_NUMBER][CITY_NUMBER];
-	public ArrayList<Chromosome> Population = new ArrayList<Chromosome>();
+	public static ArrayList<Chromosome> Population = new ArrayList<Chromosome>();
+	public static int age;
+    private static void initialization() {
+        //Create a random map
+        for (int i = 0; i < CITY_NUMBER; i++){
+            for (int j = 0; j < CITY_NUMBER; j++){
+               if (i < j){
+                   MAP[i][j] = (int)(Math.random()*MAX_DISTANCE);
+                   MAP[j][i] = MAP[i][j];
+               }
+            }
+        }
+
+        //Create first generation
+        for (int i = 0; i < MAX_POPULATION; i++){
+            Population.add(new Chromosome());
+        }
+
+        age = 0;
 
 
-	//
+    }
+
+    private static void reproduction() {
+
+    }
+
+    private static void elimination() {
+        //TODO
+
+    }
+
+
+
+    public static void main(){
+		initialization();
+		for (int i = 0;i < MAX_GENERATION;i++){
+			reproduction();
+            elimination();
+            age++;
+		}
+	}
+
+
+
+
+
+    //
 	
 }
-
-
-
-
-//class Chromosome{
-//	int[] Gene =new int[GA_TSP.CITY_NUMBER];
-//	
-//}
