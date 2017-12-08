96b0d9c7f5d49b6d50280e0a953d426cd0db9ac1
Shiqi Dai (Sean) <shiqidai1002@gmail.com>
2017-12-08 17:34:47 -0500

    + eliminate() change some methods' name + TODO in Chromosome.java

--- src/geneticAlgorithms/Chromosome.java
+++ src/geneticAlgorithms/Chromosome.java
@@ -3,59 +3,67 @@ package geneticAlgorithms;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Chromosome {
+public class Chromosome implements Comparable{
 
-	public final static int CITY_NUMBER = GA_TSP.CITY_NUMBER;
-	public static int[][] MAP = GA_TSP.MAP;
-	public int gene[] ;
-	public int distance;
-	/*
-	Construction
-	Create by an array
-	 */
-	public Chromosome(int[] gene) {
-		this.gene = gene.clone();
-		calDistance();
-	}
-	/*
-	Construction
-	Create by random
-	 */
-	public Chromosome() {
-		Random random = new Random();
-		gene = new int[CITY_NUMBER];
-		for (int i = 1; i < CITY_NUMBER; i++){
-			gene[i] = i;
-		}
-		int j = 0;
-		int t = 0;
-		for (int i = CITY_NUMBER-1; i > 0; i--){
-			j = random.nextInt(i)+1;
-			t = gene[i];
-			gene[i] = gene [j];
-			gene[j] = t;
-		}
-		calDistance();
+    public final static int CITY_NUMBER = GA_TSP.CITY_NUMBER;
+    public static int[][] MAP = GA_TSP.MAP;
+    public int gene[];
+    public int distance;
+
+    /*
+    Construction
+    Create by an array
+     */
+    public Chromosome(int[] gene) {
+        this.gene = gene.clone();
+        calDistance();
+    }
+
+    /*
+    Construction
+    Create by random
+     */
+    public Chromosome() {
+        Random random = new Random();
+        gene = new int[CITY_NUMBER];
+        for (int i = 1; i < CITY_NUMBER; i++) {
+            gene[i] = i;
+        }
+        int j = 0;
+        int t = 0;
+        for (int i = CITY_NUMBER - 1; i > 0; i--) {
+            j = random.nextInt(i) + 1;
+            t = gene[i];
+            gene[i] = gene[j];
+            gene[j] = t;
+        }
+        calDistance();
 
 
-	}
-	/*
-	Calculate distance
-	 */
-	private void calDistance(){
-		distance = 0;
-		for (int i = 1;i < CITY_NUMBER; i++){
-			distance += MAP[gene[i-1]][gene[i]];
-		}
-		distance += MAP[gene[CITY_NUMBER-1]][0];
-	}
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Chromosome that = (Chromosome) o;
-		return Arrays.equals(gene, that.gene);
-	}
+    /*
+    Calculate distance
+     */
+    private void calDistance() {
+        distance = 0;
+        for (int i = 1; i < CITY_NUMBER; i++) {
+            distance += MAP[gene[i - 1]][gene[i]];
+        }
+        distance += MAP[gene[CITY_NUMBER - 1]][0]; //TODO WENQI: return to the origin??
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Chromosome that = (Chromosome) o;
+        return Arrays.equals(gene, that.gene);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Chromosome that = (Chromosome) o;
+        return this.distance - that.distance;
+    }
 }
--- src/geneticAlgorithms/GA_TSP.java
+++ src/geneticAlgorithms/GA_TSP.java
@@ -1,33 +1,32 @@
 package geneticAlgorithms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class GA_TSP {
 
-	public static final int CITY_NUMBER = 10;
-	public static final int MAX_GENERATION = 100;
-	public static final int MAX_POPULATION = 100;
-	public static final int MAX_DISTANCE = 30000;
-	public static final double MUTATION_POSSIBILITY = 0.01;
-	public static int[][] MAP = new int[CITY_NUMBER][CITY_NUMBER];
-	public static ArrayList<Chromosome> population = new ArrayList<Chromosome>();
-	public static int age;
+    public static final int CITY_NUMBER = 10;
+    public static final int MAX_GENERATION = 100;
+    public static final int MAX_POPULATION = 100;
+    public static final int MAX_DISTANCE = 30000;
+    public static final double MUTATION_POSSIBILITY = 0.01;
+    public static int[][] MAP = new int[CITY_NUMBER][CITY_NUMBER];
+    public static ArrayList<Chromosome> population = new ArrayList<Chromosome>();
+    public static int age;
     private static Random random = new Random();
-    private static void initialization() {
+
+    private static void initialize() {
         //Create a random map
-        for (int i = 0; i < CITY_NUMBER; i++){
-            for (int j = 0; j < CITY_NUMBER; j++){
-               if (i < j){
-                   MAP[i][j] = (int)(Math.random()*MAX_DISTANCE);
-                   MAP[j][i] = MAP[i][j];
-               }
+        for (int i = 0; i < CITY_NUMBER; i++) {
+            for (int j = 0; j < CITY_NUMBER; j++) {
+                if (i < j) {
+                    MAP[i][j] = (int) (Math.random() * MAX_DISTANCE); //TODO Wenqi: Math.random() can never be 1.0
+                    MAP[j][i] = MAP[i][j];
+                }
             }
         }
 
         //Create first generation
-        for (int i = 0; i < MAX_POPULATION; i++){
+        for (int i = 0; i < MAX_POPULATION; i++) {
             population.add(new Chromosome());
         }
 
@@ -35,31 +34,31 @@ public class GA_TSP {
 
     }
 
-    private static void reproduction() {
+    private static void reproduce() {
 
         //Selection
         int[] order = new int[MAX_POPULATION];
-        for (int i = 0; i < order.length; i++){
+        for (int i = 0; i < order.length; i++) {
             order[i] = i;
         }
         int t = 0;
         int j = 0;
-        for (int i = MAX_POPULATION - 1; i > 0; i++){
-            j = random.nextInt(i+1);
+        for (int i = MAX_POPULATION - 1; i > 0; i++) {
+            j = random.nextInt(i + 1);
             t = order[i];
             order[i] = order[j];
             order[j] = t;
         }
 
         //Reproduction
-        for (int i = 0; i < MAX_POPULATION - 1; i += 2){
-            population.addAll(Arrays.asList(copulation(population.get(order[i]), population.get(order[i + 1]))));
+        for (int i = 0; i < MAX_POPULATION - 1; i += 2) {
+            population.addAll(Arrays.asList(copulate(population.get(order[i]), population.get(order[i + 1]))));
         }
 
 
     }
-    private static Chromosome[] copulation(Chromosome a,Chromosome b){
 
+    private static Chromosome[] copulate(Chromosome a, Chromosome b) {
 
 
         int sp1 = random.nextInt(CITY_NUMBER);       //start of chromosome fragment
@@ -69,19 +68,19 @@ public class GA_TSP {
             sp1 = sp2;
             sp2 = t;
         }
-        return copulation(a,b,sp1,sp2);
+        return copulate(a, b, sp1, sp2);
     }
 
-    public static Chromosome[] copulation(Chromosome a,Chromosome b, int sp1, int sp2){
-       Chromosome[] decendant = new Chromosome[2];
+    public static Chromosome[] copulate(Chromosome a, Chromosome b, int sp1, int sp2) {
+        Chromosome[] descendant = new Chromosome[2];
 
-       int[] gene_1 = new int[CITY_NUMBER];        //first son
-       int[] gene_2 = new int[CITY_NUMBER];        //second son
+        int[] gene_1 = new int[CITY_NUMBER];        //first child
+        int[] gene_2 = new int[CITY_NUMBER];        //second child
 
-       //Copulation
+        //copulate
         boolean[] hash_1 = new boolean[CITY_NUMBER];      //record used gene(visited city)
         boolean[] hash_2 = new boolean[CITY_NUMBER];
-        for (int i = sp1; i<= sp2; i++){
+        for (int i = sp1; i <= sp2; i++) {
             gene_1[i] = a.gene[i];
             hash_1[a.gene[i]] = true;
             gene_2[i] = b.gene[i];
@@ -89,8 +88,8 @@ public class GA_TSP {
         }
         int ka = 0;
         int kb = 0;
-        for (int i = 0; i < CITY_NUMBER; i++){
-            if ((i >= sp1)&&(i <= sp2)) continue;
+        for (int i = 0; i < CITY_NUMBER; i++) {
+            if ((i >= sp1) && (i <= sp2)) continue;
 
             while (hash_1[b.gene[kb]]) kb++;
             gene_1[i] = b.gene[kb];
@@ -105,31 +104,57 @@ public class GA_TSP {
         //Mutation
 
         //return
-        decendant[0] = new Chromosome(gene_1);
-        decendant[1] = new Chromosome(gene_2);
-        return decendant;
+        descendant[0] = new Chromosome(gene_1);
+        descendant[1] = new Chromosome(gene_2);
+        return descendant;
     }
 
-    private static void elimination() {
+    private static int calWorstDistance() {
+        int worstDistance = 0;
         //TODO
+        return worstDistance;
+    }
 
+    private static boolean theDeath(int percentage) {
+        random = new Random();
+        return random.nextInt(100) < percentage;
+    }
+
+    private static void eliminate() {
+        // sorting
+        Collections.sort(population); //ascending order
+
+        // remove by chance
+        int deadChance;
+        ArrayList<Chromosome> deadList = new ArrayList<Chromosome>();
+        while (deadList.size() < 100) {
+            for (int i = population.size() - 1; i >= 0; i--) {
+                if (deadList.size() < 100) {
+                    deadChance = population.get(i).distance / calWorstDistance() * 100;
+                    if (theDeath(deadChance)) {
+                        deadList.add(population.get(i));
+                    }
+                } else {
+                    break;
+                }
+            }
+            for (Chromosome dying : deadList) {
+                population.remove(dying);
+            }
+        }
     }
 
 
-
-    public static void main(){
-		initialization();
-		for (int i = 0;i < MAX_GENERATION;i++){
-			reproduction();
-            elimination();
+    public static void main() {
+        initialize();
+        for (int i = 0; i < MAX_GENERATION; i++) {
+            reproduce();
+            eliminate();
             age++;
-		}
-	}
-
-
-
+        }
+    }
 
 
     //
-	
+
 }
