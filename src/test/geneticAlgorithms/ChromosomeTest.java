a6a7a3eed0fc912ad19e85fe0487879958a78d8b
Shiqi Dai (Sean) <shiqidai1002@gmail.com>
2017-12-09 16:36:27 -0500

    update ChromosomeTest.java

--- src/test/geneticAlgorithms/ChromosomeTest.java
+++ src/test/geneticAlgorithms/ChromosomeTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.After;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 /**
@@ -32,7 +33,13 @@ public class ChromosomeTest {
      */
     @Test
     public void testEquals() throws Exception {
-//TODO: Test goes here... 
+        int[] gene1 = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 1};
+        int[] gene2 = {0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 2, 19};
+        Chromosome cm1 = new Chromosome(gene1);
+        Chromosome cm2 = new Chromosome(gene2);
+        assertFalse(cm1.equals(cm2));
+        Chromosome cm3 = new Chromosome(gene1);
+        assertTrue(cm1.equals(cm3));
     }
 
     /**
@@ -57,7 +64,13 @@ public class ChromosomeTest {
      */
     @Test
     public void testMutation() throws Exception {
-//TODO: Test goes here... 
+        int[] gene1 = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 1};
+        int[] gene2 = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 1};
+        Chromosome cm1 = new Chromosome(gene1);
+        Chromosome cm2 = new Chromosome(gene2);
+        assertTrue(cm1.equals(cm2));
+        cm2.mutation();
+        assertFalse(cm1.equals(cm2));
     }
 
 
